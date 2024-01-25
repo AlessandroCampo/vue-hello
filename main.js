0cf1e8d12436b355f738a758249d6d67251c1dfd
AlessandroCampo <alessandrocampo97@gmail.com>
2024-01-25 16:01:07 +0100

    feat: h1 and img

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            image: "https://picsum.photos/seed/picsum/400/300"
+        }
+    }
+}).mount('#app')
